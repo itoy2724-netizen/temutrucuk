4e7091e02d66f236b52557bb350cf6616d1e2adb
itoy2724-netizen <itoy2724@gmail.com>
2026-07-02 01:12:55 +0300

    Update Connection.php

--- api/gmypanel/Connection.php
+++ api/gmypanel/Connection.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
 $servername = getenv('DB_HOST') ?: "localhost";
 $username   = getenv('DB_USER') ?: "root1";
